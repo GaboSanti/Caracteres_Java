1fbcba26129932ffdea3b991ee7a6b064f8f1df7
GaboSanti <20243rd011@utez.edu.mx>
2025-05-06 09:16:01 -0600

    Caracteres en java

---
+++ Caracteres.java
@@ -0,0 +1,12 @@
+public class Caracteres{
+    public static void main(String[] args){
+        char letra = 'J';
+        char simbolo = '@';
+        char numero = '5';
+
+        system.out.println("letra = " + letra)
+        system.out.println("simbolo = " + simbolo)
+        system.out.println("numero = " + numero)
+
+    }
+}
